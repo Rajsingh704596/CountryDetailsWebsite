f5eeba72e4b3316fc9f843b6ea12ec48fd3357f0
Raj <rajsingh704596@gmail.com>
2025-01-16 18:53:20 +0530

    change the code

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,4 +1,5 @@
 import { FaArrowCircleRight } from "react-icons/fa";
+import { NavLink } from "react-router-dom";
 
 function Home() {
   return (
@@ -8,9 +9,8 @@ function Home() {
         <div className="hero-section">  
           <h2>Discover the Wonders of Earth</h2>
           <p className="h-content">Explore continents, countries, and cultures with a touch of a button. Our World Atlas showcases geographical information like never before—detailed, dynamic, and interactive. Get insights into population data, climate patterns, and geographical features, all presented in an engaging and visually rich format.</p>
-          <button className="btn"> Start Exploring<FaArrowCircleRight /></button>
+          <NavLink to="/country"> <button className="btn"> Start Exploring<FaArrowCircleRight /></button> </NavLink>
         </div>
-
         <div>
         <img src="./image/world.jpg" alt="World picture" />
         </div>
